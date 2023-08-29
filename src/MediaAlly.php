3a9a57c2039e098e34d5ce9b5cb1283d4e2410ec
billvog <billvog04@gmail.com>
2023-08-29 18:24:06 +0300

    Fixed detachMedia function in MediaAlly.php to delete single asset

--- src/MediaAlly.php
+++ src/MediaAlly.php
@@ -12,14 +12,12 @@ use CloudinaryLabs\CloudinaryLaravel\Model\Media;
  * Whether the model should automatically reload its media relationship after modification.
  *
  */
-trait MediaAlly
-{
+trait MediaAlly {
 
     /**
      * Relationship for all attached media.
      */
-    public function medially()
-    {
+    public function medially() {
         return $this->morphMany(Media::class, 'medially')->orderBy('order', 'asc');
     }
 
@@ -27,9 +25,8 @@ trait MediaAlly
     /**
      * Attach Media Files to a Model
      */
-    public function attachMedia($file, $order = 0, $options = [])
-    {
-        if(! file_exists($file)) {
+    public function attachMedia($file, $order = 0, $options = []) {
+        if (!file_exists($file)) {
             throw new Exception('Please pass in a file that exists');
         }
 
@@ -40,7 +37,7 @@ trait MediaAlly
         $media->file_url = $response->getSecurePath();
         $media->size = $response->getSize();
         $media->file_type = $response->getFileType();
-				$media->order = $order;
+        $media->order = $order;
 
         $this->medially()->save($media);
     }
@@ -48,8 +45,7 @@ trait MediaAlly
     /**
      * Attach Rwmote Media Files to a Model
      */
-    public function attachRemoteMedia($remoteFile, $order = 0, $options = [])
-    {
+    public function attachRemoteMedia($remoteFile, $order = 0, $options = []) {
         $response = resolve(CloudinaryEngine::class)->uploadFile($remoteFile, $options);
 
         $media = new Media();
@@ -57,70 +53,69 @@ trait MediaAlly
         $media->file_url = $response->getSecurePath();
         $media->size = $response->getSize();
         $media->file_type = $response->getFileType();
-				$media->order = $order;
+        $media->order = $order;
 
         $this->medially()->save($media);
     }
 
     /**
-    * Get all the Media files relating to a particular Model record
-    */
-    public function fetchAllMedia()
-    {
+     * Get all the Media files relating to a particular Model record
+     */
+    public function fetchAllMedia() {
         return $this->medially()->get();
     }
 
     /**
-    * Get the first Media file relating to a particular Model record
-    */
-    public function fetchFirstMedia()
-    {
+     * Get the first Media file relating to a particular Model record
+     */
+    public function fetchFirstMedia() {
         return $this->medially()->first();
     }
 
     /**
-    * Delete all/one file(s) associated with a particular Model record
-    */
-    public function detachMedia(Media $media = null)
-    {
+     * Delete all/one file(s) associated with a particular Model record
+     */
+    public function detachMedia(Media $media = null) {
+        // If $media is null the expected behavior is to
+        // delete all the media attached to our model
+        // from cloudinary and our database.
+        if (is_null($media)) {
+            $items = $this->medially()->get();
 
-       $items = $this->medially()->get();
-
-        foreach($items as $item) {
-            resolve(CloudinaryEngine::class)->destroy($item->getFileName());
-
-            if (!is_null($media) && $item->id == $media->id) {
-                return $item->delete();
+            foreach ($items as $item) {
+                resolve(CloudinaryEngine::class)->destroy($item->getFileName());
             }
-        }
 
-        return $this->medially()->delete();
+            return $this->medially()->delete();
+        }
+        // Otherwise delete only the specified media from cloudinary 
+        // and our database.
+        else {
+            resolve(CloudinaryEngine::class)->destroy($media->getFileName());
+            return $media->delete();
+        }
     }
 
     /**
-    * Get the last Media file relating to a particular Model record
-    */
-    public function fetchLastMedia()
-    {
+     * Get the last Media file relating to a particular Model record
+     */
+    public function fetchLastMedia() {
         return $this->medially()->get()->last();
     }
 
     /**
-    * Update the Media files relating to a particular Model record
-    */
-    public function updateMedia($file, $options = [])
-    {
+     * Update the Media files relating to a particular Model record
+     */
+    public function updateMedia($file, $options = []) {
         $this->detachMedia();
         $this->attachMedia($file, $options);
     }
 
     /**
-    * Update the Media files relating to a particular Model record (Specificially existing remote files)
-    */
-    public function updateRemoteMedia($file, $options = [])
-    {
+     * Update the Media files relating to a particular Model record (Specificially existing remote files)
+     */
+    public function updateRemoteMedia($file, $options = []) {
         $this->detachMedia();
         $this->attachRemoteMedia($file, $options);
     }
-
 }
